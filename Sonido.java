2daaef8bb99d2fa5a585cbdbf592477451153a69
Perju <yohanam.paulus@gmail.com>
2017-03-08 23:16:02 +0100

    Update Sonido.java

--- Sonido.java
+++ Sonido.java
@@ -1,20 +1,23 @@
 package pruebas;
-
+// imports necesarios
 import java.io.InputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
-public class Sonido {
-
+public class SonidoJavaIO {
+	// Una clase que te pide un string para usarlo como ruta
 	private InputStream path;
 
-	public Sonido(String path){
-		this.path = Prueba004.class.getResourceAsStream(path); 
+	public SonidoJavaIO(String path){
+		// hay que usar el getResource de la clase principal
+		// usando el .class. para poder acceder a las cosas que heredan todas las clases
+		this.path = MainClass.class.getResourceAsStream(path);
 	}
 
 	public void reproducirSonido(int tiempo) throws InterruptedException{
 		try
 		{
+			//cargamos el sonido y empezamos a reproducirlo
 			Clip sonido = AudioSystem.getClip();
 			sonido.open(AudioSystem.getAudioInputStream(this.path));
 			sonido.start();
@@ -23,6 +26,7 @@ public class Sonido {
 		{
 			System.out.println(fallo);
 		}
+		// para esperar el tiempo necesario hasta que se reproduzca el sonido en milisegundos.
 		Thread.sleep(tiempo);
 	}
 }
