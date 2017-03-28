a63f5073cef0eb58396b0d0b38399df51eff00d2
Perju <yohanam.paulus@gmail.com>
2017-03-29 01:05:13 +0200

    Add files via upload

--- excepciones/dni/Principal.java
+++ excepciones/dni/Principal.java
@@ -1,34 +1,22 @@
-package dni;
+package excepciones;
 
 import java.util.Scanner;
 
 public class Principal {
-	public static void comporbarDNI(String dni) throws DNIFormatoCadenaIncorrecto{
-		//aprovechar la captura de excepciones para lanzar la mia propia
-		try{
-			//si la longitud del DNI no es correcta osea 9 generamos un error
-			if(dni.length()!=9){Integer.parseInt("a");}
-			// intentamos convertir los 8 primero caracteres a numeros y si no son numeros se genera el error solo
-			Integer.parseInt(dni.substring(0, 7));
-			if((dni.charAt(8)>'z' || dni.charAt(8)<'a') && (dni.charAt(8)>'Z' || dni.charAt(8)<'A')){
-				Integer.parseInt("a");//si no termina en una letra generamos un error
-			}
-		}catch(Exception e){
-			// le pasamos una cadena al constructor de la Excepcion
-			throw new DNIFormatoCadenaIncorrecto("formato incorrecto de la cadena");
-		}
-	}
 
 	public static void main(String[] args) {
-		System.out.println("Dame un dni");
+		
 		String dni;
 		boolean salir=false;
 		do{
+			// pedimos datos dentro de un bucle infinito del que no podemos salir
 			Scanner kb = new Scanner(System.in);
+			System.out.println("Dame un dni");
 			dni=kb.nextLine();
+			// capturamos nuestro propio error
 			try{
-				comporbarDNI(dni);
-			}catch(DNIFormatoCadenaIncorrecto e){
+				DNI.comporbarDNI(dni);
+			}catch(DNIException e){
 				e.printStackTrace();
 			}
 		}while(!salir);
