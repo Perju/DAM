51cca2d02b399565b648ea59d968d15abcbc31bf
Perju <yohanam.paulus@gmail.com>
2017-03-29 01:05:31 +0200

    Delete DNIFormatoCadenaIncorrecto.java

--- excepciones/dni/DNIFormatoCadenaIncorrecto.java
+++
@@ -1,8 +0,0 @@
-package dni;
-
-public class DNIFormatoCadenaIncorrecto extends Exception {
-	DNIFormatoCadenaIncorrecto(String cadena){
-		// pasamos una cadena a la clase Extends y al pintar el stacktrace parece un error profesional XD
-		super(cadena);
-	}
-}
