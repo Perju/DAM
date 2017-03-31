105ffab52306fa4de2bcc176c38f103c3ba05600
Perju <yohanam.paulus@gmail.com>
2017-03-31 14:23:27 +0200

    Edicion de comentarios
    Se editan los comentarios para que sean mas correctos

--- Sonido.java
+++ Sonido.java
@@ -5,20 +5,22 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class SonidoJavaIO {
-	// Una clase que te pide un string para usarlo como ruta
+	// Una clase que te usa string para usarlo como ruta del archivo que queremos cargar como un stream de bits
 	private InputStream path;
 
 	public SonidoJavaIO(String path){
 		// hay que usar el getResource de la clase principal
-		// usando el .class. para poder acceder a las cosas que heredan todas las clases
+		// usando el .class. para poder acceder los metodos heredados de java, del objeto en ejecución.
 		this.path = MainClass.class.getResourceAsStream(path);
 	}
 
 	public void reproducirSonido(int tiempo) throws InterruptedException{
+		// capturamos excepciones
 		try
 		{
-			//cargamos el sonido y empezamos a reproducirlo
+			// Obtenemos un clip de sonido del sistema
 			Clip sonido = AudioSystem.getClip();
+			// Cargamos el archivo como un stream de bits
 			sonido.open(AudioSystem.getAudioInputStream(this.path));
 			sonido.start();
 		}
@@ -27,6 +29,7 @@ public class SonidoJavaIO {
 			System.out.println(fallo);
 		}
 		// para esperar el tiempo necesario hasta que se reproduzca el sonido en milisegundos.
+		// esta excepcion no se captura ya que solo se produce en aplicaciones con varios hilos de ejecución
 		Thread.sleep(tiempo);
 	}
 }
